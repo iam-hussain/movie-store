812aa43231e4189079435975d413590e3d961dac
Zakir Hussain <44015638+ZaHuPro@users.noreply.github.com>
2019-08-29 08:54:58 +0530

    Production Server

--- Client/src/config/production.js
+++
@@ -1,6 +0,0 @@
-module.exports = { 
-    server : "http://localhost:1234/",
-    GraphQL : "http://localhost:1234/graphql",
-    uploadURL: "http://localhost:1234/upload",
-    file_name: "image"
-}
